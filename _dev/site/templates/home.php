1a9bbd0b51c78da6c43ff159e9f82f2c47237452
Elliot Smith <elljsmee@gmail.com>
2017-02-08 10:59:11 +0000

    about to start js experiments

--- _dev/site/templates/home.php
+++ _dev/site/templates/home.php
@@ -11,9 +11,10 @@
           Hi, my name is Elliot - I like to take photos, design websites and writing about technology.
         </h2>
         <h2 class="intro--type">You should keep scrolling down to take a look at all the stuff I get up to. Don’t be afraid to contact me if you have any photographic or web design work!</h2>
-
       </div>
     </article>
+
+    <hr />
 
     <article class="photography">
 
